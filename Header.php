4651bcc7dc99545e3fb3b5d9704b54b42a78b9a9
amineminoop <31425930+amineminoop@users.noreply.github.com>
2020-10-07 23:54:35 +0200

    header
    header

---
+++ Header.php
@@ -0,0 +1,57 @@
+<div class="header">
+    <div class="header__left">
+      <img
+        src="https://scontent-mrs2-1.xx.fbcdn.net/v/t1.15752-0/p280x280/120481143_940539346449608_123390524307015704_n.png?_nc_cat=110&_nc_sid=ae9488&_nc_ohc=0GJJEl3IjZsAX-EvV9F&_nc_ht=scontent-mrs2-1.xx&oh=2623c6fb2fdf844e39a4414bf675b6ea&oe=5FA2469F"
+        alt=""
+      />
+      <div class="header__input">
+        <i class="fa fa-search"></i>
+        <input placeholder="Search" type="text" />
+      </div>
+    </div>
+    <div class="header__center">
+      <div class="header__option activee">
+        <i class="fa fa-home large"></i>
+      </div>
+
+   
+      <div class="header__option">
+        <i class="fa fa-envelope large"></i>
+      </div>
+      <div class="header__option">
+        <i class="fa fa-bell large"></i>
+      </div>
+    </div>
+
+  
+
+    <div class="header__right">
+        <div class="header__info">
+          <img src="https://scontent-mrs2-1.xx.fbcdn.net/v/t1.0-1/p100x100/116686525_1598368613675432_1700674936674760310_o.jpg?_nc_cat=110&_nc_sid=dbb9e7&_nc_ohc=9Aqyt-EQGP0AX-gvPV6&_nc_ht=scontent-mrs2-1.xx&tp=6&oh=45cc81f98b95d456a3539864c913c041&oe=5FA25722" alt="" srcset="">
+          <h4>ziyaadddd</h4>
+        </div>
+    </div>
+  </div>
+  <!-- navbar mobile -->
+  <div class="header__center__mobile">
+
+ 
+    <div class="header__option">
+     <i class="fa fa-search large"></i>
+    </div>
+ 
+    <div class="header__option">
+      <i class="fa fa-envelope large"></i>
+    </div>
+
+    <div class="header__option activee">
+      <i class="fa fa-home large"></i>
+    </div>
+
+    <div class="header__option">
+      <i class="fa fa-bell large"></i>
+    </div>
+    <div class="header__info">
+      <img src="https://scontent-mrs2-1.xx.fbcdn.net/v/t1.0-1/p100x100/116686525_1598368613675432_1700674936674760310_o.jpg?_nc_cat=110&_nc_sid=dbb9e7&_nc_ohc=9Aqyt-EQGP0AX-gvPV6&_nc_ht=scontent-mrs2-1.xx&tp=6&oh=45cc81f98b95d456a3539864c913c041&oe=5FA25722" alt="" srcset="">
+    </div>
+  </div>
